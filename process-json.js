13e8570f7036bf4a0aee81ad99f15b84294d8682
matucasal <matias.casal@yahoo.com.ar>
2023-02-02 13:53:55 +0100

    features template

--- process-json.js
+++ process-json.js
@@ -1,7 +1,20 @@
 function processJson(json){
 	const nombre = json.IdentificacionConsultada[0].NombreSujeto;
 	const documento = json.IdentificacionConsultada[0].NumeroDocumentoDobleInfo;
-	return {nombre, documento}
+	const historico360 = {
+		'actual' : json['Historico Score 360'].find(x => x.Retroactivo === "Actual").Score,
+		'6meses' : json['Historico Score 360'].find(x => x.Retroactivo === "-6 Meses").Score,
+		'12meses' : json['Historico Score 360'].find(x => x.Retroactivo === "-12 Meses").Score,
+	}
+
+	const resumenInforme = {
+		'opeImpagos' : json['Datos Resumen Informe 360'].find(x => x.Tipo === "OpeImpagos"),
+		'demandaJudicial' : json['Datos Resumen Informe 360'].find(x => x.Tipo === "DemandaJudicial"),
+		'carteraCastigada' : json['Datos Resumen Informe 360'].find(x => x.Tipo === "CarteraCastigada"),
+	}
+
+	console.log(resumenInforme);
+	return {nombre, documento, historico360, resumenInforme}
 }
 
 module.exports = processJson;
